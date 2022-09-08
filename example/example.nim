9d5e10ba98bbd875cdd8719f00c4a594180bff0f
Adam Múdry <mudry.ado@gmail.com>
2022-09-08 14:34:39 +0200

    renamed bitfield to bitseq

--- example/example.nim
+++ example/example.nim
@@ -1,5 +1,5 @@
 import std/[sequtils, sugar]
-import bitfields
+import bitseqs
 
 #              MSB -> 0101 1100 <- LSB
 let number: uint8 = 0b0101_1100
@@ -17,17 +17,17 @@ assert number.bits().toSeq() == @[0.Bit, 1, 0, 1, 1, 1, 0, 0] # `toSeq` is from 
 #                  0  or            1   == 1
 assert (number.bit(1) or number.bit(2)) == 1.Bit
 
-# Create `BitField` (seq[Bit])
+# Create `BitSeq` (seq[Bit])
 # Indexed from MSB (left side)
 #        0123 4567
 # MSB -> 0101 1100 <- LSB
-let bf = number.toBitField()
+let bf = number.toBitSeq()
 assert bf == @[0.Bit, 1, 0, 1, 1, 1, 0, 0]
 
-# Get data back from `BitField` as uint8 - uint64
-assert fromBitfield[uint8](bf) == 0b0101_1100
+# Get data back from `BitSeq` as uint8 - uint64
+assert fromBitSeq[uint8](bf) == 0b0101_1100
 
-# Use any seq operations on `BitField`
+# Use any seq operations on `BitSeq`
 assert bf.filterIt(it == 1.Bit).len == 4
 
 # Get a seq of indexes of bits set to 1/0
@@ -40,19 +40,19 @@ assert bf.bitsIdxOn.toSeq() == @[1, 3, 4, 5]
 # MSB -> 0101 1100 <- LSB
 assert bf.bitsIdxOffSeq() == @[0, 2, 6, 7]
 
-# Create a new empty `BitField`
-var bf2 = newBitField(uint8) # == newBitField(8)
+# Create a new empty `BitSeq`
+var bf2 = newBitSeq(uint8) # == newBitSeq(8)
 for i in 0..<4:
   bf2[i] = 1.Bit
 assert bf2 == @[1.Bit, 1, 1, 1, 0, 0, 0, 0]
 
-# Perform bitwise operations on `BitFields`
+# Perform bitwise operations on `BitSeqs`
 assert (bf and bf2) == @[0.Bit, 1, 0, 1, 0, 0, 0, 0]
 assert (bf xor bf2) == @[1.Bit, 0, 1, 0, 1, 1, 0, 0]
 var bf3 = not bf2
 assert bf3 == @[0.Bit, 0, 0, 0, 1, 1, 1, 1]
 
-# Flip `Bits` in `BitFields`
+# Flip `Bits` in `BitSeqs`
 bf2.bitFlip(0)
 assert bf2 == @[0.Bit, 1, 1, 1, 0, 0, 0, 0]
 bf2.bitFlip(0..3)
@@ -65,7 +65,7 @@ assert bf3 == @[0.Bit, 0, 0, 1, 0, 0, 0, 0]
 # With `dup()` from std/sugar it doesn't mutate original
 assert bf3.dup(bitFlip(0)) == @[1.Bit, 0, 0, 1, 0, 0, 0, 0]
 
-# Set `Bits` in `BitFields`
+# Set `Bits` in `BitSeqs`
 bf3.bitSet(0, 1)
 assert bf3 == @[1.Bit, 0, 0, 1, 0, 0, 0, 0]
 bf3.bitSet(1..3, 1)
@@ -73,7 +73,7 @@ assert bf3 == @[1.Bit, 1, 1, 1, 0, 0, 0, 0]
 bf3.bitSet(^3..^1, 1)
 assert bf3 == @[1.Bit, 1, 1, 1, 0, 1, 1, 1]
 
-# Resize `BitFields`
+# Resize `BitSeqs`
 # If `size` is smaller than `bf.len`, the most significant bits are dropped
 # If `size` is larger than `bf.len`, the most significant bits are padded with 0
 bf3.resize(4)
